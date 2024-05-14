a6049d33d138b16cf26e0922acc453ec78c19402
Oussama Galai <132763749+MO5ITO@users.noreply.github.com>
2024-05-15 00:17:13 +0100

    WeatherAPI

--- WeatherAPI/src/App.jsx
+++ WeatherAPI/src/App.jsx
@@ -1,10 +1,14 @@
+import loader from './assets/loader.svg'
+import browser from './assets/browser.svg'
+
 function App() {
  
   return (
 
       <main>
       <h1>Weather App</h1>
-      
+      <img src={loader} alt="loader" />
+      <img src={browser} alt="browser" />
       </main>
   
   );
