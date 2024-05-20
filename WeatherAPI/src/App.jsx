292b87efca75c1db241c96af1dc6fa51ad10de38
Oussama Galai <132763749+MO5ITO@users.noreply.github.com>
2024-05-20 21:39:24 +0100

    WeatherAPI

--- WeatherAPI/src/App.jsx
+++ WeatherAPI/src/App.jsx
@@ -1,18 +1,23 @@
 import loader from './assets/loader.svg'
 import browser from './assets/browser.svg'
+import iconWeather from '../public/icons/01d.svg'
+import './App.css'
 
 function App() {
  
   return (
 
       <main>
-
-          <h1>Weather App</h1>
           <div className="loader-container">
             <img src={loader} alt="loader" />
-            <img src={browser} alt="browser" />
+            {/* <img src={browser} alt="browser" /> */}
           </div>
-
+          <p className="city-name">Paris</p>
+          <p className="country-name">France</p>
+          <p className="temperature">17</p>
+          <div className="info-icon-container">
+            <img src={iconWeather} alt="Weather Icon" className='info-icon'/>
+          </div>
       </main>
   
   );
