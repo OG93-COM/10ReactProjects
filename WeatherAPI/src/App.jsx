926ea1d5ec6216f8eee8867d5555470cdcad7e69
Oussama Galai <132763749+MO5ITO@users.noreply.github.com>
2024-05-21 01:32:17 +0100

    WeatherAPI

--- WeatherAPI/src/App.jsx
+++ WeatherAPI/src/App.jsx
@@ -7,31 +7,36 @@ import { useEffect } from 'react'
 const APIKEY = import.meta.env.VITE_WEATHER_API_KEY;
 
 function App() {
-
+  const [weatherData,setWeatherData] = useState(null);
   const [countryName, setCountryName] = useState(null);
   const [cityName, setCityName] = useState('');
   const [temperature, setTemperature] = useState(0);
   const [iconWeather, setIconWeather] = useState('');
-
+  
+  const [errorInfo,setErrorInfo] = useState(null);
 
   useEffect(()=>{
     fetch(`http://api.airvisual.com/v2/nearest_city?key=${APIKEY}`)
     .then (res => {
-      return res.json()
+      if (!res.ok) throw new Error(`error ${res.status} - ${res.statusText}`);
+      return res.json();
+      
     })
     .then (resData => {
+      setWeatherData(resData)
       setCountryName(resData.data.country);
       setCityName(resData.data.city);
       setTemperature(resData.data.current.weather.tp);
       setIconWeather(resData.data.current.weather.ic)
     }).catch(error => {
       console.error('Error fetching weather data:', error);
+      setErrorInfo(error.message);
     });
   },[]) 
   return (
 
       <main>
-          <div className={`loader-container ${!countryName && "active"} `}>
+          <div className={`loader-container ${(!weatherData && !errorInfo) && "active"} `}>
             <img src={loader} alt="loader" />
             {/* <img src={browser} alt="browser" /> */}
           </div>
@@ -45,6 +50,13 @@ function App() {
             </div>
           </> )
           }
+
+          {(errorInfo && !countryName) && (
+            <>
+            <p className='error-information'>{errorInfo}</p>
+            <img src={browser} alt='error Icon'/>
+            </>
+          )}
           
           
       </main>
