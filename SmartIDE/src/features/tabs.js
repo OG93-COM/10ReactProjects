471b43ec3f5d6962983e68861c27a6aeca392f84
Oussama Galai <132763749+MO5ITO@users.noreply.github.com>
2024-05-31 00:16:51 +0100

    Smart IDE

--- SmartIDE/src/features/tabs.js
+++ SmartIDE/src/features/tabs.js
@@ -1,19 +1,50 @@
 import {htmlIcon, cssIcon, jsIcon} from '../assets/index.js'
+import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = [
     {
         id:1,
-        lang:"HTML",
-        imageUrl:{htmlIcon}
+        lang:"html",
+        imageUrl: htmlIcon,
+        buttonContent: "HTML",
+        code:`<div>
+        <h1>React Code Editor</h1>
+        <p>Coding Live</p>
+        </div>
+        `
     },
     {
         id:2,
-        lang:"CSS",
-        imageUrl:{cssIcon}
+        lang:"css",
+        imageUrl: cssIcon,
+        buttonContent: "CSS",
+        code:`body{
+            font-family:Roboto, sans-serif;
+            padding: 25px;
+            color:#111;
+            background-color: #f1f1f1;
+        }
+        `
     },
     {
         id:3,
-        lang:"JS",
-        imageUrl:{jsIcon}
+        lang:"js",
+        imageUrl: jsIcon ,
+        buttonContent: "JavaScript",
+        code:`console.log('Hello World');
+        `
     }
 ]
+
+export const codeUpdater = createSlice({
+    name:'code-updater',
+    initialState,
+    reducers:{
+        updateCode:(state,action) =>{
+            state.find(obj => obj.id === action.payload.id).code = action.payload.value
+        }
+    }
+})
+
+export const {updateCode} = codeUpdater.actions;
+export default codeUpdater.reducer;
