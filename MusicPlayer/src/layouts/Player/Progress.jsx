b50bffc757c02eaf1ee2ecd63629dca1d10c1842
Oussama Galai <132763749+MO5ITO@users.noreply.github.com>
2024-06-21 21:22:50 +0100

    MusicPlayer

--- MusicPlayer/src/layouts/Player/Progress.jsx
+++ MusicPlayer/src/layouts/Player/Progress.jsx
@@ -5,13 +5,24 @@ import getFormattedValues from '../../utils/getFormattedValues'
 const Progress = () => {
   const progressData = useSelector(state => state.progress)
 
+  const handleProgressClick = (e) => {
+    const player = document.getElementById('audio-player')
+    const rect = e.target.getBoundingClientRect()
+    const widht = rect.widht
+    const x = e.clientX - rect.left
+    player.currentTime = (x / widht) * progressData.totalDuration
+  }
   return (
     <div className='max-w-[800px] mx-auto'>
-      <div className='bg-slate-900 h-2 rounded cursor-pointer'>
-        <div className='bg-indigo-400 scale-x-50 origin-left h-full pointer-events-none'></div>
+      <div
+      onClick={handleProgressClick}
+      className='bg-slate-900 h-2 rounded cursor-pointer'>
+        <div 
+        style={{transform:`scaleX(${progressData.current / progressData.totalDuration})`}}
+        className='bg-indigo-400 origin-left h-full pointer-events-none'></div>
       </div>
       <div className='flex justify-between items-center text-gray-800 font-light text-sm mt-1 mx-1'>
-        <span>{getFormattedValues(progressData.current)}</span>
+        <span>{progressData.current ? getFormattedValues(progressData.current) : "00:00"}</span>
         <span>{getFormattedValues(progressData.totalDuration)}</span>
 
       </div>
