3685fe12c6623acbed25348aacde320a528a5509
Oussama Galai <132763749+MO5ITO@users.noreply.github.com>
2024-06-24 00:34:36 +0100

    Unsplash

--- UnsplashClone/src/App.jsx
+++ UnsplashClone/src/App.jsx
@@ -1,8 +1,10 @@
 
 function App() {
   return (
-    <div>
-  
+    <div className="bg-gray-50 min-h-screen">
+      <div className="max-w-4xl mx-auto pt-20 px-4">
+
+      </div>
     </div>
   )
 }
